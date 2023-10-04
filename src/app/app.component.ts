d109f2d3e6cdcc785dc1a9207af9c9682de6681f
mouradmns <mouradmannasse@gmail.com>
2023-10-04 18:20:33 +0100

    changes on accounts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ebanking_Front.ng';
+  title = 'digital-banking-web';
 }
